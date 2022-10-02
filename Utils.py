1993939bbbbb9b7885a3339cfece121af514348d
Raciniewska <raciniewska.barbara@gmail.com>
2022-10-02 17:19:04 +0200

    make plots prettier

--- Utils.py
+++ Utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from past.utils import old_div
 from scipy import linalg
+from os.path import splitext
 
 
 def _col_major_2darray(X):
@@ -41,3 +42,75 @@ matrix, or regularization methods.""".format(
         )
 
     return snapshots, snapshots_shape
+
+def plot_mode_2D(
+    index_mode = None,
+    filename = None,
+    x = None,
+    y = None,
+    order = "C",
+    figsize = (8, 8),
+    modes = None,
+    scales=None
+):
+    """
+Plot the DMD Modes.
+"""
+    if modes is None:
+        raise ValueError(
+            "The modes have not been computed."
+            "You have to perform the fit method."
+        )
+    xgrid, ygrid = np.meshgrid(x, y)
+
+    if index_mode is None:
+        index_mode = list(range(modes.shape[1]))
+    elif isinstance(index_mode, int):
+        index_mode = [index_mode]
+
+    if filename:
+        basename, ext = splitext(filename)
+
+    for idx in index_mode:
+        fig = plt.figure(figsize=figsize)
+        fig.suptitle("DMD wektor własny {}".format(idx))
+
+        real_ax = fig.add_subplot(1, 2, 1)
+        imag_ax = fig.add_subplot(1, 2, 2)
+
+        mode = modes.T[idx].reshape(xgrid.shape, order=order)
+
+        real = real_ax.pcolor(
+            xgrid,
+            ygrid,
+            mode.real,
+            cmap="jet",
+            vmin=scales[0][0],#mode.real.min(),
+            vmax=scales[0][1],#mode.real.max(),
+        )
+        imag = imag_ax.pcolor(
+            xgrid,
+            ygrid,
+            mode.imag,
+            vmin=scales[1][0],#mode.imag.min(),
+            vmax=scales[1][1],#mode.imag.max(),
+        )
+
+        fig.colorbar(real, ax=real_ax)
+        fig.colorbar(imag, ax=imag_ax)
+
+        real_ax.set_aspect("auto")
+        imag_ax.set_aspect("auto")
+
+        real_ax.set_title("Część rzeczywista")
+        imag_ax.set_title("Część urojona")
+
+        # padding between elements
+        plt.tight_layout(pad=2.0)
+
+        if filename:
+            plt.savefig("{0}.{1}{2}".format(basename, idx, ext))
+            plt.close(fig)
+
+    if not filename:
+        plt.show()
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import scipy.integrate
 from matplotlib import pyplot as plt
 from pydmd import DMD
 from rDMD.rDMDClass import rDMDClass
-from Utils import _col_major_2darray
+from Utils import _col_major_2darray, plot_mode_2D
 import time as tm
 
 #Generowanie szeregów czasowych + nakładanie szumu
@@ -17,16 +17,20 @@ snapshots = [d+n for d,n in zip(data, noise)]
 
 #Wizualizacja wejściowych szeregów czasowych
 fig = plt.figure(figsize=(18,12))
+plt.rc('font', size=20)
 for id_subplot, snapshot in enumerate(snapshots, start=1):
-    plt.subplot(4, 4, id_subplot)
+    sub = plt.subplot(4, 4, id_subplot)
+    sub.set(title="Migawka nr." + str(id_subplot))
     plt.pcolor(x1grid, x2grid, snapshot.real, vmin=-1, vmax=1)
-plt.suptitle("Input snapshots",fontsize=40)
+plt.subplots_adjust(wspace=0.5, hspace=0.5)
+plt.suptitle("Wygenerowane migawki czasowe",fontsize=40)
 plt.show()
 # The `svd_rank` can be set to zero for an automatic selection of the truncation rank;
 # in some cases (as this tutorial) the singular values should be examinated in order to select the proper truncation.
 fig = plt.figure(figsize=(18,12))
 plt.plot(scipy.linalg.svdvals(np.array([snapshot.flatten() for snapshot in snapshots]).T), 'o')
-plt.title("Singular values of flatten input snapshots",fontsize=40)
+plt.title("Wartości własne 'spłaszczonej' macierzy wejściowej",fontsize=40)
+#plt.title("Singular values of flatten input snapshots",fontsize=40)
 plt.show()
 
 #Deterministyczny DMD na szeregu czasowym
@@ -36,12 +40,20 @@ plt.show()
 # - `opt`: boolean flag that allows to choose between the standard version and the optimized one.
 dmd = DMD(svd_rank=1, tlsq_rank=0, exact=True, opt=True)
 dmd.fit(snapshots)
-dmd.plot_modes_2D(figsize=(12,5))
+#dmd.plot_modes_2D(figsize=(12,5))
 
 #Randomizowany DMD na szeregu czasowym
 rdmd = rDMDClass(svd_rank=1, tlsq_rank=0, exact=True, opt=False)
 rdmd.fit(snapshots, oversample = 12,n_subspace=2, random_state =None)
-rdmd.plot_modes_2D(figsize=(12,5))
+#rdmd.plot_modes_2D(figsize=(12,5))
+
+#Plotting
+idx=0
+plot_scales = [[-0.04, 0.02],[-0.02,0.03]]
+plot_mode_2D(x=np.arange(dmd._snapshots_shape[0]), y=np.arange(dmd._snapshots_shape[1]),
+                   figsize=(12,5), modes=dmd.modes, scales=plot_scales,index_mode =idx)
+plot_mode_2D(x=np.arange(rdmd._snapshots_shape[0]), y=np.arange(rdmd._snapshots_shape[1]),
+                   figsize=(12,5), modes=rdmd.modes,scales=plot_scales,index_mode =idx)
 
 
 #MSE w porównaniu z danymi wejściowymi
@@ -108,18 +120,22 @@ rdmd.fit(snapshots, oversample = 12,  n_subspace=2, random_state=None)
 #rekonstrukcja stanów deterministyczny DMD
 fig = plt.figure(figsize=(18,12))
 for id_subplot, snapshot in enumerate(dmd.reconstructed_data.T, start=1):
-      plt.subplot(4, 4, id_subplot)
+      sub = plt.subplot(4, 4, id_subplot)
       plt.pcolor(x1grid, x2grid, snapshot.reshape(x1grid.shape).real, vmin=-1, vmax=1)
-plt.suptitle("Reconstructed snapshots with deteministic DMD",fontsize=40)
+      sub.set(title="Migawka nr." + str(id_subplot))
+plt.subplots_adjust(wspace=0.5, hspace=0.5)
+plt.suptitle("Rekonstrukcja migawek czasowych przez deterministyczny algorytm DMD",fontsize=30)
 plt.show()
 
 #rekonstrukcja stanów randomizowany DMD
 fig = plt.figure(figsize=(18,12))
 fig = plt.figure(figsize=(18,12))
 for id_subplot, snapshot in enumerate(rdmd.reconstructed_data.T, start=1):
-      plt.subplot(4, 4, id_subplot)
+      sub = plt.subplot(4, 4, id_subplot)
       plt.pcolor(x1grid, x2grid, snapshot.reshape(x1grid.shape).real, vmin=-1, vmax=1)
-plt.suptitle("Reconstructed snapshots with randomized DMD",fontsize=40)
+      sub.set(title="Migawka nr." + str(id_subplot))
+plt.subplots_adjust(wspace=0.5, hspace=0.5)
+plt.suptitle("Rekonstrukcja migawek czasowych przez randomizowany algorytm DMD",fontsize=30)
 plt.show()
 
 #Bład rekonstrukcji deterministyczny DMD
@@ -134,9 +150,12 @@ dmd.dmd_time['tend'] *= 3
 print("Shape after manipulation: {}".format(dmd.reconstructed_data.shape))
 
 fig = plt.figure(figsize=(18,12))
+plt.rc('font', size=10)
 for id_subplot, snapshot in enumerate(dmd.reconstructed_data.T, start=1):
-     plt.subplot(7, 7, id_subplot)
+     sub = plt.subplot(7, 7, id_subplot)
      plt.pcolor(x1grid, x2grid, snapshot.reshape(x1grid.shape).real, vmin=-1, vmax=1)
+     sub.set(title="Migawka nr." + str(id_subplot))
+plt.subplots_adjust(wspace=0.5, hspace=0.5)
 plt.show()
 
 # # For a check about the accuracy of the reconstruction, we can plot the integral computed on the original snapshots and on the DMD states.
@@ -145,10 +164,10 @@ dmd_states = [state.reshape(x1grid.shape) for state in dmd.reconstructed_data.T]
 original_int = [compute_integral(compute_integral(snapshot)).real for snapshot in snapshots]
 dmd_int = [compute_integral(compute_integral(state)).real for state in dmd_states]
 figure = plt.figure(figsize=(18, 5))
-plt.plot(dmd.original_timesteps, original_int, 'bo', label='original snapshots')
-plt.plot(dmd.dmd_timesteps, dmd_int, 'r.', label='dmd states')
-plt.ylabel('Integral')
-plt.xlabel('Time')
+plt.plot(dmd.original_timesteps, original_int, 'bo', label='oryginalne migawki czasowe')
+plt.plot(dmd.dmd_timesteps, dmd_int, 'r.', label='wektory własne')
+plt.ylabel('Całka')
+plt.xlabel('Czes')
 plt.grid()
 leg = plt.legend()
 plt.show()
@@ -161,8 +180,10 @@ print("Shape after manipulation: {}".format(rdmd.reconstructed_data.shape))
 
 fig = plt.figure(figsize=(18,12))
 for id_subplot, snapshot in enumerate(rdmd.reconstructed_data.T, start=1):
-     plt.subplot(7, 7, id_subplot)
+     sub = plt.subplot(7, 7, id_subplot)
      plt.pcolor(x1grid, x2grid, snapshot.reshape(x1grid.shape).real, vmin=-1, vmax=1)
+     sub.set(title="Migawka nr." + str(id_subplot))
+plt.subplots_adjust(wspace=0.5, hspace=0.5)
 plt.show()
 
 # # For a check about the accuracy of the reconstruction, we can plot the integral computed on the original snapshots and on the DMD states.
@@ -171,10 +192,10 @@ rdmd_states = [state.reshape(x1grid.shape) for state in rdmd.reconstructed_data.
 original_int = [compute_integral(compute_integral(snapshot)).real for snapshot in snapshots]
 rdmd_int = [compute_integral(compute_integral(state)).real for state in rdmd_states]
 figure = plt.figure(figsize=(18, 5))
-plt.plot(rdmd.original_timesteps, original_int, 'bo', label='original snapshots')
-plt.plot(rdmd.dmd_timesteps, rdmd_int, 'r.', label='rdmd states')
-plt.ylabel('Integral')
-plt.xlabel('Time')
+plt.plot(rdmd.original_timesteps, original_int, 'bo', label='oryginalne migawki czasowe')
+plt.plot(rdmd.dmd_timesteps, rdmd_int, 'r.', label='wektory własne')
+plt.ylabel('Całka')
+plt.xlabel('Czes')
 plt.grid()
 leg = plt.legend()
 plt.show()
@@ -190,9 +211,11 @@ for i in range(0,15):
     prev_frame = next_frame
 predictions =np.concatenate((flatten_A.T,np.array(predictions)))
 for id_subplot, snapshot in enumerate(predictions, start=1):
-     plt.subplot(7, 7, id_subplot)
+     sub=plt.subplot(7, 7, id_subplot)
      plt.pcolor(x1grid, x2grid, snapshot.reshape(x1grid.shape).real, vmin=-1, vmax=1)
-plt.suptitle("Prediction snapshots with deterministic DMD", fontsize=40)
+     sub.set(title="Migawka nr." + str(id_subplot))
+plt.suptitle("Przewidywane migawki czasowe przez deterministyczny algorytm DMD", fontsize=30)
+plt.subplots_adjust(wspace=0.5, hspace=0.5)
 plt.show()
 
 #Predykcja przyszłości przez niedeterministyczny DMD
@@ -205,8 +228,10 @@ for i in range(0,15):
     prev_frame = next_frame
 predictions =np.concatenate((flatten_A.T,np.array(predictions)))
 for id_subplot, snapshot in enumerate(predictions, start=1):
-     plt.subplot(7, 7, id_subplot)
+     sub=plt.subplot(7, 7, id_subplot)
      plt.pcolor(x1grid, x2grid, snapshot.reshape(x1grid.shape).real, vmin=-1, vmax=1)
-plt.suptitle("Prediction snapshots with randomized DMD", fontsize=40)
+     sub.set(title="Migawka nr." + str(id_subplot))
+plt.suptitle("Przewidywane migawki czasowe przez randomizowany algorytm DMD", fontsize=30)
+plt.subplots_adjust(wspace=0.5, hspace=0.5)
 plt.show()
 print("finito")
